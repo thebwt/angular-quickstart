1b947bafc6d8d4233d1134ed84a26693f48637e7
Brandon Tomlinson <Brandon.Tomlinson@rackspace.com>
2016-10-05 17:55:29 -0500

    crud done

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -17,7 +22,9 @@ import {DashboardComponent} from "./dashboard.component";
     imports:      [
         BrowserModule,
         routing,
-        FormsModule
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
     ],
     declarations: [
         AppComponent,
